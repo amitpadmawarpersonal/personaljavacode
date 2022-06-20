f1a30a170a0072c83ba76384bb8a97826a0461d4
apadmawar <apadmawar@xento.com>
2022-06-20 12:57:30 +0530

    new changes in git class

--- src/gittestpackage/GitClass.java
+++ src/gittestpackage/GitClass.java
@@ -1,9 +1,13 @@
 package gittestpackage;
 
 public class GitClass {
-	
-	public GitClass () {
+
+	public GitClass() {
 		System.out.println("this is default const");
+	}
+
+	static {
+		System.out.println("This is statis block");
 	}
 
 	public static void main(String[] args) {
