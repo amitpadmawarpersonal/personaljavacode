c56b8dad3892a42c55b3495e42a100f23aaa648c
apadmawar <apadmawar@xento.com>
2022-06-20 12:05:21 +0530

    adding changes back

--- src/gittestpackage/GitClass.java
+++ src/gittestpackage/GitClass.java
@@ -1,6 +1,10 @@
 package gittestpackage;
 
 public class GitClass {
+	
+	public GitClass () {
+		System.out.println("this is default const");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
